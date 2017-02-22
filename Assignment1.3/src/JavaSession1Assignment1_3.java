ca4e51f742813b362a3f514605d052d84c3be177
Sandeep Hegde <sandeep.hegde123@gmail.com>
2017-02-23 00:41:54 +0530

    Java Session 1 Assignment 1.3
    Session 1 Assignment 1.3 of Core Java Course

---
+++ Assignment1.3/src/JavaSession1Assignment1_3.java
@@ -0,0 +1,66 @@
+
+//Write a program that swaps the values of two variables without using third variable.
+
+
+
+public class JavaSession1Assignment1_3 {
+
+	
+	public static void main (String[] args) 
+	     
+	     {
+	    	 
+	        swap s1 = new swap();
+	    	 s1.SwapDisplay();
+	    	 
+	     }
+	     
+	
+}
+
+class swap {
+	
+	
+      int a ; 
+      int b ;
+      int temp;
+    
+
+swap()
+
+{
+	  
+	  a = 10;
+	  b = 20;
+	  temp = a;
+	  a = b;
+	  b = temp;
+	  
+}
+
+
+
+void SwapDisplay()
+
+{
+	  
+	   
+	  System.out.println("Before Swapping");
+      System.out.println("Value of a is = " + a);
+      System.out.println("Value of b is = " + b);
+	  
+	  a = a + b;
+	  b = a - b;
+	  a = a - b;
+	  
+	  System.out.println("After Swapping");
+      System.out.println("Value of a is = " + a);
+      System.out.println("Value of b is = " + b);
+	  
+	  
+}
+
+
+}
+
+
